f0ce4a0ffba3419b31bd8ca6d5be19e47c5b8c21
Gulnara <gulnarakhasanova82@gmail.com>
2026-02-11 20:36:06 +0300

    add theme toggle button and mouseover header event

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+const recolorFirstCardButton= document.getElementById('recolor-first-card-button');
+const firstCatalogCard = document.querySelector('.card-container')
+const blueHashColor = "#0000FF";
+
+recolorFirstCardButton.addEventListener('click', () => {
+firstCatalogCard.style.backgroundColor = blueHashColor;
+})
+
+const greenHashColor = '#00ff44';
+
+const catalogCards = document.querySelectorAll('.card-container');
+const recolorAllCardButton = document.getElementById('recolor-all-card-button');
+
+recolorAllCardButton.addEventListener('click', () => {
+  catalogCards.forEach(
+    card => card.style.backgroundColor = greenHashColor
+)
+})
+
+const googleOpenButton = document.getElementById('open-google-button');
+googleOpenButton.addEventListener('click', openGoogle);
+
+function openGoogle() {
+  const answer = confirm('Вы действительно хотите перейти на сайт Google?')
+
+  if (answer === true) {
+    window.open('https://google.com');
+  } else {
+    console.log('Пользователь отменил переход на Google')
+  }
+  }
+
+  const mainHeader = document.querySelector(' title');
+  mainHeader.addEventListener('mouseover', () => {
+    console.log('mainHeader.textContent');
+  });
+
+  const toggleBtn = document.getElementById('theme-toggle-button');
+  toggleBtn,addEventListener('click', () => {
+  toggleBtn.classList.toggle('active');
+  });
+  
