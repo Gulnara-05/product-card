83babf4a7dd8b68024bf771fb8b0ad2530081456
Gulnara <gulnarakhasanova82@gmail.com>
2026-02-21 22:41:39 +0300

    fixed array objects

--- homework-6.js
+++ homework-6.js
@@ -9,47 +9,53 @@ const person = {
   city: "Барселона",
   status: "Не замужем",
   isWorking: true
-  } ;
+  };
 console.log("Файл подключен! Мой объект:", person);
 
-const myCar ={
+const myCar = {
   brand: "Porsche",
   model: "911 Carrera S",
   year: 2024,
   color: "Frozen Berry Metallic",
   transmission: "PDK",
   interior: "Leather Chalk",
-} ;
+};
 myCar.owner = person;
-console.log("Моя машина:",myCar);
+console.log("Моя машина:", myCar);
 
-function addSpeed(car) {
+function addMaxSpeedValue(car) {
   if (!("maxSpeed" in car)) {
     car.maxSpeed = 310;
     console.log("Скорость Porsche", car.maxSpeed);
   }
 }
-addSpeed(myCar);
+addMaxSpeedValue(myCar);
 
-function showValue(obj, key) {
+function showObjectValue(obj, key) {
   console.log(key + ":", obj[key]);
 }
-showValue(myCar, "model");
-showValue(person, "position");
-showValue(person, "city");
+showObjectValue(myCar, "model");
+showObjectValue(person, "position");
+showObjectValue(person, "city");
 
 const products = ["Клубника", "Авокадо", "Сыр", "Шоколад"];
 console.log("(Продукты):", products);
 
 const myMovies = [
-  { title: "Побег из Шоушенка", year: 2011},
-  { title: "По соображением совести", year: 2016},
-  { title: "Кровавый алмаз", year: 2006},
+  {
+   title: "Побег из Шоушенка", year: 2011,
+  },
+  {
+   title: "По соображением совести", year: 2016,
+  },
+  {
+   title: "Кровавый алмаз", year: 2006,
+  },
 ];
-myMovies.push({ title: "Зелёная миля", year: 1999})
+myMovies.push({ title: "Зелёная миля", year: 1999,});
 console.log("Фильмы:", myMovies);
 
-const extraFilm = [{ title: "Оппенгеймер", year: 2023}];
+const extraFilm = [{title: "Оппенгеймер", year: 2023,}];
 const allFilms = [...myMovies, ...extraFilm];
 console.log("Все фильмы вместе:", allFilms);
 
