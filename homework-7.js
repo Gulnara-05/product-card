e908e44a4a6c380921a7ef0216c26839abd4534a
Gulnara <gulnarakhasanova82@gmail.com>
2026-03-09 19:42:36 +0300

    export, import and array processing

---
+++ homework-7.js
@@ -0,0 +1,40 @@
+import { socialComments } from './comments.js';
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const filteredNumbers = numbers.filter(num => num >= 5);
+console.log('Уровень 1.2 (Числа >=5):', filteredNumbers);
+
+const kitchenAppliances = ['Тостер', 'Чайник', 'Миксер', 'Кофемашина', 'Блендер'];
+const hasMixer = kitchenAppliances.includes('Миксер');
+console.log('Уровень 1.3 (Есть ли миксер?):', hasMixer);
+
+function reverseArray(arr) {
+  return [...arr].reverse();
+}
+console.log('Уровень 1.4(наоборот):', reverseArray(kitchenAppliances));
+
+const comEmails = socialComments.filter(comment => comment.email.includes('.com'));
+console.log('7. Автолюбители .com:', comEmails);
+
+const updatedPosts = socialComments.map(comment => ({
+    ...comment,
+    postId: comment.id <= 5 ? 2 : 1
+}));
+console.log('8. Обновленные postId:', updatedPosts);
+
+const simpleList = socialComments.map(comment => ({
+    id: comment.id,
+    name: comment.name
+}));
+console.log('9. Краткий список имен:', simpleList);
+
+const validatedComments = socialComments.map(comment => ({
+    ...comment,
+    isInvalid: comment.body.length > 180
+}));
+console.log('10. Проверка длины постов:', validatedComments);
+
+const emailsAll = socialComments.map(c => c.email);
+console.log('11. Список всех почт:', emailsAll);
+
+console.log('12. Все почты одной строкой:', emailsAll.join(','));
