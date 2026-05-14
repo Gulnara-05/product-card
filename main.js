44f8414d5d785299d9df9998eedefe7bc8752bfa
Gulnara <gulnarakhasanova82@gmail.com>
2026-05-14 10:33:54 +0300

    fixed errors

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 import './cosmetics.js';
-import Modal from './Modal.js';
-import Form from './Form.js';
-
-const registrationModal = new Modal('regModal'); 
+import Modal from './modal.js';
+import Form from './form.js';
+const registrationModal = new Modal('regModal');
 const registrationForm = new Form('registrationForm');
+const subscribeForm = new Form('subscribeForm');
 
 const openBtn = document.getElementById('openModalBtn');
 
@@ -15,15 +15,33 @@ if (openBtn) {
 
 if (registrationForm.form) {
     registrationForm.form.addEventListener('submit', (event) => {
-        event.preventDefault(); 
+        event.preventDefault();
         
+        const values = registrationForm.getValues();
+
+        if (values.password !== values.confirmPassword) {
+            alert('Пароли не совпадают!');
+            return;
+        }
+
         if (registrationForm.isValid()) {
-            console.log('Данные формы:', registrationForm.getValues());
+            console.log('Данные формы:', values);
             registrationForm.reset();
             registrationModal.close();
             alert('Вы успешно зарегистрировались!');
         } else {
             alert('Пожалуйста, заполните все поля корректно.');
+        }
+    });
+}
+
+if (subscribeForm.form) {
+    subscribeForm.form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        
+        if (subscribeForm.isValid()) {
+            alert('Спасибо за подписку!');
+            subscribeForm.reset();
         }
     });
 }
